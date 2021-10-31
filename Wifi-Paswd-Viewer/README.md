bf7c353dc0be9fd76b3b236a25b6f06321fff48e
KarthikRaja2k1 <Karthikraja@gmail.com>
2021-10-31 22:07:26 +0530

    Lniux support added for getting saved wifi passwords

--- Wifi-Paswd-Viewer/README.md
+++ Wifi-Paswd-Viewer/README.md
@@ -1,5 +1,6 @@
 # NOTE BEFORE
 - You need to have the name of the wifi profile to get its password
+- Lniux users must run this python script with "sudo access"
 # Features of this python script
 - This script enables you to view password of a given wifi profile which you have already connected with
 - This script works independent of the 
--- Wifi-Paswd-Viewer/main.py
+++ Wifi-Paswd-Viewer/main.py
@@ -1,19 +1,26 @@
 from sys import platform
 import os
-import subprocess as sp
-print("Enter the wifi profile whose passwd you forgot: ")
+import subprocess
+print("Enter the wifi profile whose passwd you forgot: ",end="")
 name=input()
-if platform == "linux" or platform == "linux2":
-      pass # linux
+if platform == "linux" or platform == "linux2": # linux
+      #output = subprocess.getoutput(["sudo cat /etc/NetworkManager/system-connections/",name ,".nmconnection"])
+      file='/etc/NetworkManager/system-connections/{}.nmconnection'.format(name)
+      output = subprocess.getoutput(['sudo cat {}'.format(file)])
+      startingstr="psk="
 elif (platform == "darwin"):
       pass #OS X
-elif platform == "win32":
-      output = sp.getoutput(["netsh" ,"wlan","show","profile",name ,"key=clear"])
+elif platform == "win32":  #Windows
+      output = subprocess.getoutput(["netsh" ,"wlan","show","profile",name ,"key=clear"])
       stop=output.find('\n Cost settings')
       startingstr="Key Content            : "
-      start=output.find(startingstr)
-      for i in output[start+len(startingstr):]:
-            if(i!='\n'):
-                  print(i,end="")
-            else:
-                  break
+
+#common code
+start=output.find(startingstr)
+print("Required password is: ")
+for i in output[start+len(startingstr):]:
+      if(i!='\n'):
+            print(i,end="")
+      else:
+            break
+print()
